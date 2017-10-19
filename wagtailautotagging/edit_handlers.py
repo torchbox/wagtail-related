e77f6990df2bd4d2608add963e6c3c10c2241f43
Mikalai Radchuk <mikalai.radchuk@torchbox.com>
2017-10-19 15:49:59 +0100

    Allow to limit a number of suggested tags
    Also fixes suggestions ordering: more relevant
    suggestions appear go first.

--- wagtailautotagging/edit_handlers.py
+++ wagtailautotagging/edit_handlers.py
@@ -1,7 +1,9 @@
+import itertools
+
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, BaseFieldPanel
 
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, BaseFieldPanel
 from wagtailautotagging import get_autotagging_backend
 from wagtailautotagging.widgets import AdminTagSuggestingWidget
 
@@ -9,6 +11,7 @@ from wagtailautotagging.widgets import AdminTagSuggestingWidget
 class BaseTagSuggestingFieldPanel(BaseFieldPanel):
     field_template = 'wagtailautotagging/edit_handlers/field_panel_field.html'
     backend_name = 'default'
+    suggested_tags_limit = 10
 
     def render_as_field(self):
         backend = get_autotagging_backend(self.backend_name)
@@ -17,14 +20,17 @@ class BaseTagSuggestingFieldPanel(BaseFieldPanel):
         related_tags = None
         if self.instance and self.instance.pk:
             # Get tag suggestions
-            related_tags = set(backend.get_tags(self.instance))
+            related_tags = backend.get_tags(self.instance)
 
             # Get existing tags from a page
             obj_field = getattr(self.instance, self.field_name)
             existing_tags = set([tag.name for tag in obj_field.all()])
 
-            # Exclude tags that already exist
-            related_tags = related_tags - existing_tags
+            # Create a generator that excludes tags that already exist in the current field
+            related_tags = (element for element in related_tags if element not in existing_tags)
+
+            # Do not render all tags: use only most relevant ones
+            related_tags = itertools.islice(related_tags, self.suggested_tags_limit)
 
         context = {
             'field': self.bound_field,
@@ -35,6 +41,10 @@ class BaseTagSuggestingFieldPanel(BaseFieldPanel):
 
 
 class TagSuggestingFieldPanel(FieldPanel):
+    def __init__(self, *args, **kwargs):
+        self.suggested_tags_limit = kwargs.pop('suggested_tags_limit', None)
+        super().__init__(*args, **kwargs)
+
     def bind_to_model(self, model):
         base = {
             'model': model,
@@ -42,5 +52,7 @@ class TagSuggestingFieldPanel(FieldPanel):
             'classname': self.classname,
             'widget': AdminTagSuggestingWidget,
         }
+        if self.suggested_tags_limit:
+            base['suggested_tags_limit'] = self.suggested_tags_limit
 
         return type(str('_TagSuggestingFieldPanel'), (BaseTagSuggestingFieldPanel,), base)

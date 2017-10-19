85e987d9e348879714c8ef4b4729735dd83e8e02
Mikalai Radchuk <mikalai.radchuk@torchbox.com>
2017-10-19 14:35:19 +0100

    Remove a tag from a suggestions list after a click

--- wagtailautotagging/static/wagtailautotagging/admin_widget.js
+++ wagtailautotagging/static/wagtailautotagging/admin_widget.js
@@ -2,11 +2,17 @@ function initTagSuggestingField(id) {
     $(document).on('ready', function () {
         var $tagitField = $('#' + id);
         var $tagitFieldContainer = $tagitField.parents('.field-content');
+        var $tagSuggestionsContainer = $tagitFieldContainer.find('.tag-suggestions');
 
-        $tagitFieldContainer.find('.tag-suggestions .tag').on('click', function(e) {
+        $tagSuggestionsContainer.find('.tag').on('click', function(e) {
             e.preventDefault();
+            var tagSuggestion = $(e.target);
 
-            $tagitField.tagit('createTag', $(e.target).text());
+            $tagitField.tagit('createTag', tagSuggestion.text());
+            tagSuggestion.remove();
+            if ($tagSuggestionsContainer.find('.tag').length === 0) {
+                $tagSuggestionsContainer.remove();
+            }
         });
     });
 }

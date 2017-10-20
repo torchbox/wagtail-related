f6b02a10d4aa47ec500db7eaa22b5f076d300018
Mikalai Radchuk <mikalai.radchuk@torchbox.com>
2017-10-20 15:50:11 +0100

    Improve tag suggestion UI
    Hide a suggested tag from the "Relevant tags" area,
    when an editor adds it manually (not by clicking on a tag in the "Relevant tags" area).

--- wagtailautotagging/static/wagtailautotagging/js/admin_widget.js
+++ wagtailautotagging/static/wagtailautotagging/js/admin_widget.js
@@ -3,25 +3,53 @@ function initTagSuggestingField(id) {
         var $tagitField = $('#' + id);
         var $tagitFieldContainer = $tagitField.parents('.field-content');
         var $tagSuggestionsContainer = $tagitFieldContainer.find('.tag-suggestions');
+        var getVisibleTagSuggestions = function () {
+            return $tagSuggestionsContainer.find('.tag').filter(function() {
+                return this.style['display'] !== 'none';
+            });
+        };
 
+        // Add suggested tag into a tagit input on click
+        // and hide it form the relevant tags area.
         $tagSuggestionsContainer.find('.tag').on('click', function(e) {
             e.preventDefault();
-            var tagSuggestion = $(e.target);
+            var $tagSuggestion = $(e.target);
 
-            $tagitField.tagit('createTag', tagSuggestion.text());
-            tagSuggestion.hide();
-            if ($tagSuggestionsContainer.find('.tag:visible').length === 0) {
-                $tagSuggestionsContainer.hide();
-            }
+            $tagitField.tagit('createTag', $tagSuggestion.data('tag'));
         });
 
         $tagitField.tagit({
+            // Show a suggested tag in the relevant tags area
+            // when it's removed from a tagit input.
             beforeTagRemoved: function(event, ui) {
-                var tagLabel = ui.tagLabel.replace(/^"(.*?)"$/, "$1");
-                var tagSelector = ".tag[data-tag='" + tagLabel + "']";
+                var tagSelector = getTagSuggestionSelector(ui.tagLabel);
                 $tagSuggestionsContainer.find(tagSelector).show();
-                $tagSuggestionsContainer.show();
-            }
+
+                if (getVisibleTagSuggestions().length > 0) {
+                    $tagSuggestionsContainer.show();
+                }
+            },
+            // Hide a suggested tag in the relevant tags area,
+            // when it's added to a tagit area.
+            // This should also cover a case when user adds a tag manually.
+            beforeTagAdded: function(event, ui) {
+                var tagSelector = getTagSuggestionSelector(ui.tagLabel);
+                var $tagSuggestion = $tagSuggestionsContainer.find(tagSelector);
+
+                $tagSuggestion.hide();
+                if (getVisibleTagSuggestions().length === 0) {
+                    $tagSuggestionsContainer.hide();
+                }
+            },
+
         });
     });
+
+    function normalizeTagLabel(tagLabel) {
+        return tagLabel.replace(/^"(.*?)"$/, "$1");
+    }
+
+    function getTagSuggestionSelector(tagLabel) {
+        return ".tag[data-tag='" + normalizeTagLabel(tagLabel) + "']";
+    }
 }

091d7b2d4f1a9a0ba37fd9cbceb7a2a57a9f7bb4
Mikalai Radchuk <mikalai.radchuk@torchbox.com>
2017-11-07 16:58:10 +0000

    Update UI to get tags via http

--- wagtailautotagging/edit_handlers.py
+++ wagtailautotagging/edit_handlers.py
@@ -1,10 +1,10 @@
-import itertools
+import urllib.parse
 
 from django.template.loader import render_to_string
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, BaseFieldPanel
-from wagtailautotagging import get_autotagging_backend
 from wagtailautotagging.widgets import AdminTagSuggestingWidget
 
 
@@ -14,28 +14,19 @@ class BaseTagSuggestingFieldPanel(BaseFieldPanel):
     suggested_tags_limit = 10
 
     def render_as_field(self):
-        backend = get_autotagging_backend(self.backend_name)
-
-        # TODO: Decide if we need cache here
-        related_tags = None
-        if self.instance and self.instance.pk:
-            # Get tag suggestions
-            related_tags = backend.get_tags(self.instance.get_latest_revision_as_page())
-
-            # Get existing tags from an object
-            obj_field = getattr(self.instance, self.field_name)
-            existing_tags = set([tag.name for tag in obj_field.all()])
-
-            # Create a generator that excludes tags that already exist in the current field
-            related_tags = (element for element in related_tags if element not in existing_tags)
-
-            # Do not render all tags: use only most relevant ones
-            related_tags = list(itertools.islice(related_tags, self.suggested_tags_limit))
+        query_data = {
+            'content_type': self.instance._meta.label,
+            'field_name': self.field_name,
+            'backend_name': self.backend_name,
+            'tags_limit': self.suggested_tags_limit,
+        }
+        suggest_tags_url = reverse('wagtailautotagging:suggest_tags', args=(self.instance.pk, ))
+        suggest_tags_url += '?' + urllib.parse.urlencode(query_data)
 
         context = {
             'field': self.bound_field,
             'field_type': self.field_type(),
-            'related_tags': related_tags,
+            'suggest_tags_url': suggest_tags_url,
         }
         return mark_safe(render_to_string(self.field_template, context))
 
--- wagtailautotagging/static/wagtailautotagging/js/admin_widget.js
+++ wagtailautotagging/static/wagtailautotagging/js/admin_widget.js
@@ -1,21 +1,34 @@
 function initTagSuggestingField(id) {
     $(document).on('ready', function () {
         var $tagitField = $('#' + id);
-        var $tagitFieldContainer = $tagitField.parents('.field-content');
-        var $tagSuggestionsContainer = $tagitFieldContainer.find('.tag-suggestions');
+        var $tagitFieldContainer = $tagitField.parents('.js-field-content');
+        var $tagSuggestionsContainer = $tagitFieldContainer.find('.js-tag-suggestions');
+        var $tagSuggestionsListContainer = $tagSuggestionsContainer.find('.js-tag-suggestions--wrapper');
+        var $tagRefreshButton = $tagitFieldContainer.find('.js-tag-refresh');
+        var refreshURL = $tagRefreshButton.data('refresh-url');
+
+        // Load the data first time
+        loadData(refreshURL);
+
+        $tagRefreshButton.on('click', function (e) {
+            e.preventDefault();
+
+            loadData(refreshURL);
+        });
+
         var getVisibleTagSuggestions = function () {
-            return $tagSuggestionsContainer.find('.tag').filter(function() {
+            return $tagSuggestionsListContainer.find('.js-tag').filter(function() {
                 return this.style['display'] !== 'none';
             });
         };
 
         // Add suggested tag into a tagit input on click
         // and hide it form the relevant tags area.
-        $tagSuggestionsContainer.find('.tag').on('click', function(e) {
+        $tagSuggestionsListContainer.on('click', '.js-tag', function(e) {
             e.preventDefault();
             var $tagSuggestion = $(e.target);
 
-            $tagitField.tagit('createTag', $tagSuggestion.data('tag'));
+            $tagitField.tagit('createTag', $tagSuggestion.attr('data-tag'));
         });
 
         $tagitField.tagit({
@@ -23,33 +36,82 @@ function initTagSuggestingField(id) {
             // when it's removed from a tagit input.
             beforeTagRemoved: function(event, ui) {
                 var tagSelector = getTagSuggestionSelector(ui.tagLabel);
-                $tagSuggestionsContainer.find(tagSelector).show();
+                $tagSuggestionsListContainer.find(tagSelector).show();
 
-                if (getVisibleTagSuggestions().length > 0) {
-                    $tagSuggestionsContainer.show();
-                }
+                showOrHideTagSuggestionsContainer();
             },
             // Hide a suggested tag in the relevant tags area,
             // when it's added to a tagit area.
             // This should also cover a case when user adds a tag manually.
             beforeTagAdded: function(event, ui) {
                 var tagSelector = getTagSuggestionSelector(ui.tagLabel);
-                var $tagSuggestion = $tagSuggestionsContainer.find(tagSelector);
+                var $tagSuggestion = $tagSuggestionsListContainer.find(tagSelector);
 
                 $tagSuggestion.hide();
-                if (getVisibleTagSuggestions().length === 0) {
-                    $tagSuggestionsContainer.hide();
-                }
+                showOrHideTagSuggestionsContainer();
             },
-
         });
+
+        function loadData(loadURL) {
+            $.ajax(loadURL, {
+                success: function (data) {
+                    renderTags(data.suggested_tags);
+                }
+                // TODO: Handle errors
+            });
+        }
+
+        function renderTags(suggestedTags) {
+            var existingTags = $tagitField.tagit("assignedTags").map(function(tagLabel) {
+                return normalizeTagLabel(tagLabel);
+            });
+
+            $tagSuggestionsListContainer.empty();
+
+            suggestedTags.forEach(function (tagLabel) {
+                var $htmlTag = generateHTMLForTag(tagLabel);
+
+                if (existingTags.indexOf(tagLabel) >= 0) {
+                    // If tag is already exists we need to
+                    // append it, but hide
+                    $htmlTag.hide();
+                }
+                $tagSuggestionsListContainer.append($htmlTag);
+                // Insert space between tags to enable word wrap
+                $tagSuggestionsListContainer.append(" ");
+            });
+
+            showOrHideTagSuggestionsContainer();
+        }
+
+        function generateHTMLForTag(tagLabel) {
+            var $htmlTag = $('<span class="js-tag tag tagit-label"></span>');
+
+            // Note that we have to use the `attr` method instead of
+            // the `data` method, because we need to perform
+            // selection by data attribute later.
+            $htmlTag.attr('data-tag', tagLabel);
+            $htmlTag.text(tagLabel);
+
+            return $htmlTag;
+        }
+
+        function showOrHideTagSuggestionsContainer() {
+            if (getVisibleTagSuggestions().length === 0) {
+                $tagSuggestionsContainer.hide();
+            } else {
+                $tagSuggestionsContainer.show();
+            }
+        }
     });
 
+    // We need to normalize tag labesl because tagit
+    // returns quoted labels, if a tag has spaces
     function normalizeTagLabel(tagLabel) {
         return tagLabel.replace(/^"(.*?)"$/, "$1");
     }
 
     function getTagSuggestionSelector(tagLabel) {
-        return ".tag[data-tag='" + normalizeTagLabel(tagLabel) + "']";
+        return ".js-tag[data-tag='" + normalizeTagLabel(tagLabel) + "']";
     }
 }
